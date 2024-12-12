d043221fa5ae68c3c2874708b05beea30bd929d9
Nika <wbor777@mail.ru>
2024-12-13 00:39:30 +0300

    pr11

---
+++ pks_pr11/lib/pages/profile_related/profile_page.dart
@@ -0,0 +1,82 @@
+import 'package:pks_pr11/auth/auth_service.dart';
+import 'package:pks_pr11/pages/profile_related/logout_confirm_dialog.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+
+  final authService = AuthService();
+
+  void logout() async {
+    await authService.signOut();
+
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final userId = authService.getUserId();
+    final userEmail = authService.getUserEmail();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Профиль'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => LogoutConfirmDialog(
+                  onConfirm: () {
+                    logout();
+                    Navigator.pop(context);
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Вы вошли в аккаунт!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "ID:$userId",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                "Email:$userEmail",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+              const SizedBox(height: 60),
+            ],
+          )
+      ),
+    );
+  }
+}
---
+++ pks_pr11/lib/widgets/cart_product_card.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../cart_bloc/cart_bloc.dart';
+import '../cart_bloc/cart_event.dart';
+import '../models/product.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class CartProductCard extends StatelessWidget {
+  final Product product;
+  final int quantity;
+
+  const CartProductCard({
+    Key? key,
+    required this.product,
+    required this.quantity,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (context) {
+                context.read<CartBloc>().add(RemoveCartEvent(0, product.productId));
+              },
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Удалить',
+            ),
+          ],
+        ),
+        child: Card(
+          elevation: 3,
+          child: ListTile(
+            leading: Image.network(
+              product.imageUrl,
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+            title: Text(
+              product.name,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text('${product.price} ₽ x $quantity'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.remove),
+                  onPressed: () {
+                    if (quantity > 1) {
+                      context.read<CartBloc>().add(AddCartEvent(0, product.productId, -1));
+                    }
+                  },
+                ),
+                Text(
+                    '$quantity',
+                    style: const TextStyle(fontSize: 16),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    context.read<CartBloc>().add(AddCartEvent(0, product.productId, 1));
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
